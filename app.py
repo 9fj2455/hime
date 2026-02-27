c7fb02683227fb48a75278b052f951c44126efea
9fj2455 <cphukhang428@gmail.com>
2026-02-27 23:30:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def chat():
     return jsonify({"reply": response.text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ cấp PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
